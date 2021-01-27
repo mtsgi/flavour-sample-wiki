2de0da99e1a4dd8fcdc905f92d08f8bbb94a8909
y- <oq@live.jp>
2021-01-28 06:01:49 +0900

    Update flavour and config

--- flavour.config.js
+++ flavour.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  title: 'Sample Wiki',
+  title: '最終発表用Wiki',
   lang: 'ja',
-  description: 'This is sample wiki of "Flavour".',
+  description: '最終発表用のWikiです。自由に編集したり記事を新規作成してください。',
   style: '/assets/app.css',
   dict: {
-    newArticle: '記事作成',
-    newArticleLabel: '作成する',
-    defaultArticleTitle: '記事タイトル',
+    newArticle: '新しい記事をつくる',
+    newArticleLabel: '記事を作成！',
+    defaultArticleTitle: '＜ここに記事タイトル＞',
     defaultArticleBody: '*ここに本文を記述してください*',
-    editArticle: '記事編集',
-    editArticleLabel: '更新する',
+    editArticle: '記事を編集する',
+    editArticleLabel: '記事を更新！(編集履歴が保存されます)',
     allArticles: 'すべての記事'
   },
   markdown: {
